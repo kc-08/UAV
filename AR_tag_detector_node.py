0813c279a85bd1ee0c018cb316587c3b24f9e33b
Kailyn <kailyncai@gmail.com>
2025-07-26 20:31:27 -0400

    made compatible with cv2 4.7.0

--- AR_tag_detector_node.py
+++ AR_tag_detector_node.py
@@ -1,6 +1,6 @@
-import rcply
-from rcply.node import Node
-from camera_msgs.msg import Image
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
 from geometry_msgs.msg import Pose, Point, Quaternion
 import numpy as np
 import cv2
@@ -16,23 +16,25 @@ class ARTagDetector_Node(Node):
         self.object_points = np.array([[-self.length/2, self.length/2, 0], [self.length/2, self.length/2, 0], [self.length/2, -self.length/2, 0], [-self.length/2, -self.length/2, 0]])
         f_x, f_y = 1182.644, 1182.828
         c_x, c_y = 251.1132, 324.621
-        # self.dist_coeff = np.zeros((5,1))
         self.dist_coeff = np.array([[-0.0476501462, 1.46445018, 0.0119584430 , -0.00576903121, -6.08971241]])
         
         self.camera_matrix = np.array([[f_x, 0.0, c_x], [0.0, f_y, c_y], [0.0, 0.0, 1.0]])
 
-        aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_100)
-        parameters = cv2.aruco.DetectorParameters()
-        self.detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
+        self.aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_100)
+        # parameters = cv2.aruco.DetectorParameters()
+        self.parameters = cv2.aruco.DetectorParameters_create()
+        # self.detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
 
     def image_callback(self, msg):
         img = msg.data
-        detector = self.detector
+        aruco_dict = self.aruco_dict
+        parameters = self.parameters
         camera_matrix = self.camera_matrix
         object_points = self.object_points
         dist_coeff = self.dist_coeff
         gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        corners, ids, _ = detector.detectMarkers(gray_img)
+        corners, ids, rejected = cv2.aruco.detectMarkers(gray_img, aruco_dict, parameters = parameters)
+        # corners, ids, _ = detector.detectMarkers(gray_img)
         if ids is not None:
             self.get_logger().info(f'{len(ids)} tags Detected')
             for id, cnr in zip(ids, corners):
@@ -56,10 +58,10 @@ class ARTagDetector_Node(Node):
             self.get_logger().info('No tags Detected')
 
 def main(args = None):
-    rcply.init()
+    rclpy.init()
     node = ARTagDetector_Node()
-    rcply.spin()
-    rcply.shutdown()
+    rclpy.spin()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
